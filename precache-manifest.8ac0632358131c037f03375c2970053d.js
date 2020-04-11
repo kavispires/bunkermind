32f8c3de0d1ca87081711d7b94e612cb6f36dc60
Fabiano 'Kavis' Pires <kavispires@gmail.com>
2020-04-11 02:06:52 -0700

    Updates

---
+++ precache-manifest.8ac0632358131c037f03375c2970053d.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8035dc377d56e07afc9c0595eb30d70e",
+    "url": "/bunkermind/index.html"
+  },
+  {
+    "revision": "6f424eeac80365bb1afc",
+    "url": "/bunkermind/static/css/main.42c8619a.chunk.css"
+  },
+  {
+    "revision": "4835ea575d580afe5a9b",
+    "url": "/bunkermind/static/js/2.6be1cdbc.chunk.js"
+  },
+  {
+    "revision": "265068e7f3bc904803e029152d67e9a1",
+    "url": "/bunkermind/static/js/2.6be1cdbc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f424eeac80365bb1afc",
+    "url": "/bunkermind/static/js/main.8ff6f468.chunk.js"
+  },
+  {
+    "revision": "456db29ba85e74e839cd",
+    "url": "/bunkermind/static/js/runtime-main.fd419642.js"
+  },
+  {
+    "revision": "b014f492698321a60bfc406eec4cdc30",
+    "url": "/bunkermind/static/media/avatar-axolotl.b014f492.svg"
+  },
+  {
+    "revision": "99644b1288739f3cc5a9cf7f1c7944ab",
+    "url": "/bunkermind/static/media/avatar-cardinal.99644b12.svg"
+  },
+  {
+    "revision": "b6c20bca906902c84b3402b9e3a9a410",
+    "url": "/bunkermind/static/media/avatar-fox.b6c20bca.svg"
+  },
+  {
+    "revision": "152df38d310c8c4406cd91fdfd7d633c",
+    "url": "/bunkermind/static/media/avatar-hedgehog.152df38d.svg"
+  },
+  {
+    "revision": "035cb6d78987342ee78ee6ae2eb6385c",
+    "url": "/bunkermind/static/media/avatar-lizard.035cb6d7.svg"
+  },
+  {
+    "revision": "9cebf25b09ddc9a0b91397f2711ad304",
+    "url": "/bunkermind/static/media/avatar-mole.9cebf25b.svg"
+  },
+  {
+    "revision": "22dcc222c409bef00e1056e9a76fd4b6",
+    "url": "/bunkermind/static/media/avatar-mouse.22dcc222.svg"
+  },
+  {
+    "revision": "664f769a768bf5e5ae447b53799e04c8",
+    "url": "/bunkermind/static/media/avatar-otter.664f769a.svg"
+  },
+  {
+    "revision": "7a7e20095d51755a79111df809a7bad2",
+    "url": "/bunkermind/static/media/avatar-owl.7a7e2009.svg"
+  },
+  {
+    "revision": "77366cfe5201ca0866c415c50b6db7f4",
+    "url": "/bunkermind/static/media/avatar-platypus.77366cfe.svg"
+  },
+  {
+    "revision": "f15cecff73e0441c041dc8fc552d2387",
+    "url": "/bunkermind/static/media/avatar-rat.f15cecff.svg"
+  },
+  {
+    "revision": "9e69aff16083591db05efededcc42ab9",
+    "url": "/bunkermind/static/media/avatar-squirrel.9e69aff1.svg"
+  },
+  {
+    "revision": "b77d25564df5c39636f45f8b5f38d0e4",
+    "url": "/bunkermind/static/media/avatar-starling.b77d2556.svg"
+  },
+  {
+    "revision": "63580b9068fbdb933bdea3a4374f8cc8",
+    "url": "/bunkermind/static/media/avatar-toad.63580b90.svg"
+  },
+  {
+    "revision": "7c3b125738edf6d3c1e5fb6d7f60aed7",
+    "url": "/bunkermind/static/media/avatar-turtle.7c3b1257.svg"
+  },
+  {
+    "revision": "62489f9d5ac21a22472986e1f223fca4",
+    "url": "/bunkermind/static/media/avatar-unknown.62489f9d.svg"
+  },
+  {
+    "revision": "1eae41aa8dfbdeab271a4eec120154e3",
+    "url": "/bunkermind/static/media/bunker-mind-logo.1eae41aa.svg"
+  },
+  {
+    "revision": "110851bdd5f4c1ce0531d8d19b643e2c",
+    "url": "/bunkermind/static/media/floor-1.110851bd.svg"
+  },
+  {
+    "revision": "aceb9f1ff92c82641640be66e927da03",
+    "url": "/bunkermind/static/media/floor-2.aceb9f1f.svg"
+  },
+  {
+    "revision": "d3c82fc6ea7d45be812be333b0b5a0ca",
+    "url": "/bunkermind/static/media/floor-3.d3c82fc6.svg"
+  },
+  {
+    "revision": "752faaa7661acdeb9b1b8a1e8dd0e719",
+    "url": "/bunkermind/static/media/floor-4.752faaa7.svg"
+  },
+  {
+    "revision": "dc3c4c0de185ee8e309605f1a6841837",
+    "url": "/bunkermind/static/media/floor-5.dc3c4c0d.svg"
+  },
+  {
+    "revision": "89ba1565bf8d5b3a067e3fae9de50bb2",
+    "url": "/bunkermind/static/media/floor-6.89ba1565.svg"
+  },
+  {
+    "revision": "f567391221d8213e02b82d27d9764c31",
+    "url": "/bunkermind/static/media/floor-top.f5673912.svg"
+  }
+]);
